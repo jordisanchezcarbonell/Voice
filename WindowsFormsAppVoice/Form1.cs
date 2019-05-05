a114b07a1c29545c0c7b8d842e33b70747279ba2
jordisanchezcarbonell <47449712+jordisanchezcarbonell@users.noreply.github.com>
2019-05-05 16:06:16 +0200

    Fase 2
    He añadido funcion para restar la aplicacion ademan de que ejecute google (Probar )

--- WindowsFormsAppVoice/Form1.cs
+++ WindowsFormsAppVoice/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Speech.Synthesis;
 using System.Speech.Recognition;
+using System.Diagnostics;
 
 namespace WindowsFormsAppVoice
 {
@@ -17,12 +18,14 @@ namespace WindowsFormsAppVoice
         private string args = "Jordi";
         Choices list = new Choices();
         SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        Boolean abrir =true;
         public Form1()
         {
             
             synthesizer.SelectVoiceByHints(VoiceGender.Male); //Seleccionar la voz
             SpeechRecognitionEngine rec = new SpeechRecognitionEngine();                                                                   
-            list.Add(new String[] { "Hola", "Como estas" });
+            list.Add(new String[] { "Hola", "Como estas" , "Abre google","abrir","dormir","restart"});
+                
             Grammar gr = new Grammar(new GrammarBuilder(list));
             try
             {
@@ -47,6 +50,7 @@ namespace WindowsFormsAppVoice
         private void decir(String h)
         {
             synthesizer.Speak(h);
+            textBoxinput.AppendText(h + "\n");
         }
 
 
@@ -54,14 +58,44 @@ namespace WindowsFormsAppVoice
         {
             String result = e.Result.Text;
 
+            if (result == "abrir")
+            {
+                abrir = true;
+                labelestado.Text = "Estado: On";
+            }
+            if (result == "dormir")
+            {
+                abrir = false;
+
+                labelestado.Text = "Estado: OFF";
+            }
+            if (!abrir) { 
             //LO QUE DECIMO
             if (result == "Hola")
             {
                 // LO QUE DICE
                 decir("Hola");
             }
+                if (result == "restart")
+                {
+                    restart();
+                }
+            if(result== "Abre google")
+            {
+                Process.Start("https://www.google.es");
+            }
+
+                textBoxoutput.AppendText(result + "\n");
+
+            }
         }
 
+        private void restart()
+        {
+            Process.Start("C:\\Users\\jordi\\source\repos\\WindowsFormsAppVoice\\WindowsFormsAppVoice\bin\\Debug\\WindowsFormsAppVoice.exe");
+            Environment.Exit(0);
+
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
            //peech(args);
